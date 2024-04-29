c4ed9d1ccf58a6cf97ad714e6b1199feb34d6b22
Алексей Кожакин <alexeykozhakin@yandex.ru>
2024-04-29 09:59:44 +0200

    v.0.6 6th test

--- fridge.py
+++ fridge.py
@@ -22,18 +22,10 @@ def add_by_note(items, note):
     str_name=''
     for i in name:
         str_name+=f'{i} '
-    am_ad={}
-    am_ad['amount']= Decimal(f'{amount}')
-    am_ad['expiration_date']=datetime.datetime.strptime(date,'%Y-%m-%d').date()
-
-    if str_name[:-1] in items.keys():
-        items[str_name[:-1]].append(am_ad)
-
+    if '-' in date:
+        return add(items, str_name, amount, date)
     else:
-        food=[]
-        food.append(am_ad)
-        items[str_name[:-1]]=food
-    return items
+        return add(items, str_name, amount)
 
 
 def find(items,needle):
--- test.py
+++ test.py
@@ -35,50 +35,7 @@ goods = {
 
 
 print(amount(goods, 'яйца'))
-# Вывод: 1
+print(1)
 print(amount(goods, 'морковь'))
-# Вывод: 5
+print(5)
 
-import datetime as dt
-from decimal import Decimal
-
-goods = {
-    'Хлеб': [
-        {'amount': Decimal('1'), 'expiration_date': None},
-        {'amount': Decimal('1'), 'expiration_date': dt.date(2023, 12, 9)}
-    ],
-    'Яйца': [
-        {'amount': Decimal('2'), 'expiration_date': dt.date(2023, 12, 12)},
-        {'amount': Decimal('3'), 'expiration_date': dt.date(2023, 12, 11)}
-    ],
-    'Вода': [{'amount': Decimal('100'), 'expiration_date': None}]
-}
-
-
-
-# Вызов функции 10 декабря 2023 года
-print(expire(goods))
-# Вывод: [('Хлеб', Decimal('1'))]
-print(expire(goods, 1))
-# Вывод: [('Хлеб', Decimal('1')), ('Яйца', Decimal('3'))]
-print(expire(goods, 2))
-# Вывод: [('Хлеб', Decimal('1')), ('Яйца', Decimal('5'))]
-
-
-import datetime
-from decimal import Decimal
-
-# Холодильник пуст:
-goods = {}
-
-
-# Добавляем продукт с названием 'Яйца гусиные', количество - 4 шт.
-add_by_note(goods, 'Яйца гусиные 4 2023-07-15')
-
-# Словарь goods должен стать таким:
-
-# goods = {
-#     'Яйца гусиные': [
-#         {'amount': Decimal('4'), 'expiration_date': datetime.date(2023, 7, 15)}
-#     ]
-# }
